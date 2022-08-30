4e5900c12bc32c3d3a36e058f598d5fbf29d0d48
William <nearata@protonmail.com>
2022-08-30 18:48:59 +0200

    catch all url

---
+++ js/src/forum/index.ts
@@ -0,0 +1,29 @@
+import app from "flarum/forum/app";
+
+app.initializers.add("nearata-internal-links-noreload", () => {
+    document.addEventListener("click", (e) => {
+        const base: string = app.forum.attribute("baseUrl");
+        // @ts-ignore
+        const origin: HTMLAnchorElement = e.target.closest("a");
+
+        if (!origin) {
+            return;
+        }
+
+        if (!origin.href.startsWith(base)) {
+            return;
+        }
+
+        const path = origin.href.replace(base, "");
+        const elements = path.split("/");
+        const isFile = elements[elements.length - 1].match(/(.*)\.(.*)/i);
+
+        if (isFile) {
+            return;
+        }
+
+        e.preventDefault();
+
+        m.route.set(path);
+    });
+});
