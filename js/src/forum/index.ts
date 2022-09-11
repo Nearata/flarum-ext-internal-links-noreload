27738daaa73c20dbda20e815571b83ffc9c8fd39
William <nearata@protonmail.com>
2022-09-11 15:39:18 +0200

    ignore links that are already managed by mithril

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -2,13 +2,18 @@ import app from "flarum/forum/app";
 
 app.initializers.add("nearata-internal-links-noreload", () => {
     document.addEventListener("click", (e) => {
-        const base: string = app.forum.attribute("baseUrl");
+        if (typeof e.redraw !== "undefined") {
+            return;
+        }
+
         // @ts-ignore
         const origin: HTMLAnchorElement = e.target.closest("a");
 
         if (!origin) {
             return;
         }
+
+        const base: string = app.forum.attribute("baseUrl");
 
         if (!origin.href.startsWith(base)) {
             return;
@@ -18,8 +23,7 @@ app.initializers.add("nearata-internal-links-noreload", () => {
             return;
         }
 
-        const path = origin.href.replace(base, "");
-        const elements = path.split("/");
+        const elements = origin.pathname.split("/");
         const isFile = elements[elements.length - 1].match(/(.*)\.(.*)/i);
 
         if (isFile) {
@@ -28,6 +32,6 @@ app.initializers.add("nearata-internal-links-noreload", () => {
 
         e.preventDefault();
 
-        m.route.set(path);
+        m.route.set(origin.pathname);
     });
 });
